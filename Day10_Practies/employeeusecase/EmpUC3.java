8243250ad6f051a6be8b785a6b86e7fbd57436e8
mamtakurahe@gmail.com <deelip.markacommunications@gmail.com>
2022-02-28 21:10:46 +0530

    Day 10 practies problem commited here

---
+++ Day10_Practies/employeeusecase/EmpUC3.java
@@ -0,0 +1,74 @@
+package employeeusecase;
+
+import java.util.Scanner;
+
+public class EmpUC3 {
+	
+	public static int isfullTime= 1, ispartTime=2, workParHrRate, workFullTime, workPartTime, wages,notWork;
+	
+    public static double checkEmp;
+	
+	public static void random() {
+		 checkEmp=(int) Math.floor(Math.random() * 10) % 3;
+		 
+	}
+
+	public static void main(String[] args) {
+		
+		
+		System.out.println("employee is presnt or apsent");
+		random();
+		
+		switch((int)checkEmp){    
+	    //case statements within the switch block  
+	    case 1:
+	    	fullTimeWages(workFullTime, workParHrRate);
+	    break;    
+	    case 2:
+	    	partTimeWages(workPartTime, workParHrRate);	   	 
+	    break;    
+	    default: noPresent( notWork, workParHrRate);    
+	    }    
+		
+	}
+public static void partTimeWages(int workPartTime, int workParHrRate) {
+		
+        Scanner input = new Scanner(System.in);
+		
+		System.out.println("Enter value of PartTime employee has done");
+		workPartTime =  input.nextInt();
+		
+		System.out.println("Enter value of employee has done work as rate per hours ");
+		workParHrRate =  input.nextInt();
+		
+		int wages= workPartTime*workParHrRate;
+		System.out.println("employee work part time  and wages is " + wages);
+		
+	}
+    public static void fullTimeWages(int workFullTime, int workParHrRate) {
+		
+    	 Scanner input = new Scanner(System.in);
+ 		
+ 		System.out.println("Enter value of full time employee has done");
+ 		workFullTime =  input.nextInt();
+ 		
+ 		System.out.println("Enter value of employee has done work as rate per hours ");
+ 		workParHrRate =  input.nextInt();
+ 		
+    	int wages= workFullTime*workParHrRate;
+    	System.out.println("employee is presnt full time and wages is " + wages);	
+	}
+    public static void noPresent(int notWork, int workParHrRate) {
+    	
+    	Scanner input = new Scanner(System.in);
+    	
+    	System.out.println("Enter value of employee has done work as rate per hours ");
+  		workParHrRate =  input.nextInt();
+  		
+  		notWork = 0;
+  		
+    	int wages= notWork*workParHrRate;
+    	System.out.println("employee is presnt and wages is " + wages);	
+	}
+
+}
